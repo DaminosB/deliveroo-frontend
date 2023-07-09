08fd254ea12adb0d3f469822f1848a9e07ece183
Damien Bourcheix <damien@mbp-de-damien.home>
2023-07-09 16:13:28 +0200

    correction chemin d'accès Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./assets/components/header/header";
+import Header from "./assets/components/header/Header";
 import Content from "./assets/components/content/Content";
 
 function App() {
